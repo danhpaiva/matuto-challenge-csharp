759dd7e4c241690cd90f0c605ed0344e43d5f8b5
Paiva <danpaiva@live.com>
2022-02-14 22:59:23 -0300

    add switch case

--- challenges/Challenge001/Program.cs
+++ challenges/Challenge001/Program.cs
@@ -6,13 +6,29 @@ WriteLine("\tSorting Algorithm\n");
 int[] numbers = { 10, 2, 7, 50, 1, 9, 100, 34, 67 , 77 };
 SortArray sortArray = new();
 
-WriteLine("Original Array: ");
-sortArray.ShowArray(numbers);
+WriteLine("Choose a sorting type:\n[1]Bubble Sort [2]Merge Sort [3]Quick Sort [4]Heap Sort [X]Exit");
+string option = ReadLine().ToUpper();
 
-WriteLine("\n\n\tSorting the array using Bubble Sort\n");
-int[] arrayBubble = sortArray.BubbleSort(numbers);
+switch (option)
+{
+    case "1":
+        Clear();
+        WriteLine("Original Array: ");
+        sortArray.ShowArray(numbers);
 
-WriteLine("Sorted Array: ");
-sortArray.ShowArray(arrayBubble);
+        WriteLine("\n\n\tSorting the array using Bubble Sort\n");
+        int[] arrayBubble = sortArray.BubbleSort(numbers);
 
-ReadLine();
+        WriteLine("Sorted Array: ");
+        sortArray.ShowArray(arrayBubble);
+        ReadLine();
+        break;
+    case "X":
+        WriteLine("Thanks...Press Enter to exit...");
+        Console.ReadLine();
+        break;
+    default:
+        WriteLine("Number Invalid...\nPress Enter to exit...");
+        Console.ReadLine();
+        break;
+}
