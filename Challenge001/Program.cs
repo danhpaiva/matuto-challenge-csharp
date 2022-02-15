08f721b2ea0ba095ebb3a5174df890b1beab055c
Paiva <danpaiva@live.com>
2022-02-14 21:54:41 -0300

    refactoring code

--- Challenge001/Program.cs
+++ Challenge001/Program.cs
@@ -3,45 +3,15 @@
 WriteLine("\tSorting Algorithm\n");
 
 int[] numbers = { 10, 2, 7, 50, 1, 9, 100, 34, 67 , 77 };
+SortArray sortArray = new SortArray();
 
 WriteLine("Original Array: ");
-ShowArray(numbers);
+sortArray.ShowArray(numbers);
 
 WriteLine("\n\n\tSorting the array using Bubble Sort\n");
-int[] arrayBubble = BubbleSort(numbers);
+int[] arrayBubble = sortArray.BubbleSort(numbers);
 
 WriteLine("Sorted Array: ");
-ShowArray(arrayBubble);
+sortArray.ShowArray(arrayBubble);
 
 ReadLine();
-
-static void ShowArray(int[] arrayNumbers)
-{
-	foreach (int number in arrayNumbers)
-		Write($"{number} ");
-}
-
-static int[] BubbleSort(int[] array)
-{
-	int size = array.Length - 1;
-	int comparisonNumber = 0;
-	int changeNumber = 0;
-
-	for (int i = size; i >= 1; i--)
-	{
-		for (int j = 0; j < i; j++)
-		{
-			comparisonNumber++;
-			if (array[j] > array[j + 1])
-			{
-				int aux = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = aux;
-				changeNumber++;
-			}
-		}
-	}
-
-	return array;
-}
-
